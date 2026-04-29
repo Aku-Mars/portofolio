b1f0a40821120486a4c2656a9cf400b3d87a3610
Aku-Mars <marifinsyam73@gmail.com>
2026-04-29 18:01:09 +0700

    [+] Update Protofolio

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Fetch view count from the database
-    fetch('api/counter.php')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('view-count').innerText = data.count;
-        })
-        .catch(error => console.error('Error fetching view count:', error));
-});
